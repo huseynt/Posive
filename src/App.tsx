8cb1978a06d12e17ca4d9c6c82070884806a3207
Huseyn Tapdigli <74926067+huseynt@users.noreply.github.com>
2024-07-10 17:29:37 +0400

    loader_done

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,38 @@ import Login from './components/pages/features/Login/Login'
 import Registr from './components/pages/features/Register/Registr'
 import Forgot from './components/pages/features/Forgot/Forgot'
 import { BrowserRouter as Router } from 'react-router-dom'
-import {
-  Routes,
-  Route
-} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import { useEffect, useState } from 'react'
 import TriangleLoader from './components/common/Loader/Triangle'
 
 function App() {
 
-    // ------- loader for test -------
-  const [check, setCheck] = useState(true)
+  // ---------- loader ----------
+  const [loaded, setLoaded] = useState(false);
   useEffect(() => {
-    setTimeout(() => {
-      setCheck(false)
-    }, 800)
-  }, [])
-  // ---------------------------------
-
+    const handleStateChange = () => {
+      setLoaded(document.readyState === "complete");
+    };
+    document.addEventListener('readystatechange', handleStateChange);
+    return () => {
+      document.removeEventListener('readystatechange', handleStateChange);
+    };
+  }, []);
+  // ---------- loader ----------
 
   return (
     <div className={style.app}>
-              {check && <TriangleLoader />}
-
-    <Router>
-      <Routes>
-          <Route path='/registr' element={ <Registr/> }/>
-          <Route path='*' element={ <Login/>}/>
-          <Route path='/forgot' element={ <Forgot/> }/>
-      </Routes>
-    </Router>
+      {!loaded ? <TriangleLoader /> :
+        <Router>
+          <Routes>
+            <Route path='/registr' element={<Registr />} />
+            <Route path='*' element={<Login />} />
+            <Route path='/forgot' element={<Forgot />} />
+          </Routes>
+        </Router>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/pages/features/Register/RegistrDescription/RegistrDescription.tsx
+++ src/components/pages/features/Register/RegistrDescription/RegistrDescription.tsx
@@ -26,6 +26,9 @@ export default function Test() {
   }, []);
 
 
+  
+
+
 
 
   return (
