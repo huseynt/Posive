67075d34246627831af912a030068922f54a1e13
Huseyn Tapdigli <74926067+huseynt@users.noreply.github.com>
2024-07-11 18:15:02 +0400

    home_main_up

---
+++ src/components/pages/Home/Aside/Aside.tsx
@@ -0,0 +1,11 @@
+import style from './aside.module.scss'
+
+const Aside = () => {
+  return (
+    <div className={style.aside}>
+      sdsdsd
+    </div>
+  )
+}
+
+export default Aside
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
 import style from './home.module.scss'
 import Sidebar from '../features/Sidebar/Sidebar'
+import Main from './Main/Main'
+import Aside from './Aside/Aside'
 
 const Home = () => {
   return (
     <div className={style.home}>
       <Sidebar />
+      <Main/>
+      <Aside/>
     </div>
   )
 }
