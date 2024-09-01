d6178a21eb6b44f524b658bfabce46e748e2b267
Huseyn Tapdigli <74926067+huseynt@users.noreply.github.com>
2024-09-01 22:58:57 +0400

    add_userPermissions&Billings

--- src/components/pages/Home/Settings/UserPermissions/UserPermissions.tsx
+++ src/components/pages/Home/Settings/UserPermissions/UserPermissions.tsx
@@ -1,5 +1,6 @@
+import UserItem from '../../../features/UserItem/UserItem'
 import style from './userpermissions.module.scss'
-
+import { users } from '../../../../test/db/users'
 interface IGeneral {
   setMobileSelect: React.Dispatch<React.SetStateAction<boolean>>
 }
@@ -15,15 +16,43 @@ const UserPermissions: React.FC<IGeneral> = (props) => {
     onClick={() => setMobileSelect(false)}>
       
     <div className={style.parent_up}>
-      <h2 className={style.parent_up_head}>General</h2>
-      <h5 className={style.parent_up_info}>Update your business persona</h5>
+      <h2 className={style.parent_up_head}>User Permissions</h2>
+      <h5 className={style.parent_up_info}>Manage who has access in your system</h5>
     </div>
 
     <div className={style.parent_line}></div>
 
 
     <div className={style.parent_main}>
-      loading...
+      
+      <table className={style.parent_main_table}>
+        <thead className={style.parent_main_table_thead}>
+          <tr className={style.parent_main_table_thead_row}>
+            <th className={style.parent_main_table_thead_row_item}>
+              <input type="checkbox"/>
+            </th>
+            <th className={style.parent_main_table_thead_row_item}>No</th>
+            <th className={style.parent_main_table_thead_row_item}>Name</th>
+            <th className={`${style.parent_main_table_thead_row_item} ${style.hide}`}>Email</th>
+            <th className={`${style.parent_main_table_thead_row_item} ${style.hide}`}>Created</th>
+            <th className={style.parent_main_table_thead_row_item}>Role</th>
+            <th className={style.parent_main_table_thead_row_item}>Action</th>
+          </tr>
+        </thead>
+
+        <tbody className={style.parent_main_table_tbody}>
+          {
+            users.map((item) => (
+              <UserItem key={item.id}
+              {...item}
+              />
+            ))
+          }
+        </tbody>
+
+      </table>
+
+
     </div>
 
 
